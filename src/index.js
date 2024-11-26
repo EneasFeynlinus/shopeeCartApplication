df1f9dbb92e07c3187e2eaef26431481575055c3
Eneas Junior <eneas.silva.jr@gmail.com>
2024-11-26 12:54:01 -0300

    function createItem declared to return a object given information and subtotal being exported

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
+import createItem from "./services/itens.js";
+
 const cart = [];
+
+console.log("Welcome to your Shopee Cart!");
+
+const item1 = await createItem("Hotwhell ferrari", 20.99, 1);
+const item2 = await createItem("Hotwheels lamborghini", 39.99, 3);
+
+console.log(item2.subtotal());
